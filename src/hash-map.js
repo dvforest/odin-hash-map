f8659b9eab1af69dec84b33fd040782b7aeded05
dvforest <dvforest@hotmail.com>
2026-02-16 22:07:02 -0500

    Start implementing resize() and set(0 methods (still not finished)

--- src/hash-map.js
+++ src/hash-map.js
@@ -1,3 +1,5 @@
+import { LinkedList } from './linked-list.js';
+
 /**
  * A hash map implementation.
  */
@@ -5,6 +7,8 @@ class HashMap {
     constructor() {
         this.capacity = 16;
         this.loadFactor = 0.75;
+        this.size = 0;
+        this.buckets = Array.from({ length: capacity }, () => new LinkedList());
     }
 
     /**
@@ -23,5 +27,43 @@ class HashMap {
         }
 
         return hashCode;
+    }
+
+    /**
+     * Assigns a value to a key and store it in the hash map.
+     *
+     * @param {string} key - The name of the key.
+     * @param {string} value - The value assigned to the key.
+     */
+    set(key, value) {
+        // If the new hash map size reaches the loadFactor, resize.
+        if (this.size + 1 / this.capacity >= this.loadFactor) {
+            this.resize();
+        }
+
+        // Generate hash code and bucket for the current key
+        const hashCode = this.hash(key);
+        const bucket = hashCode % capacity;
+
+        // Update size
+        this.size += 1;
+    }
+
+    resize() {
+        // Temporarily store old buckets
+        const oldBuckets = this.buckets;
+
+        // Double capacity and create new buckets
+        this.capacity *= 2;
+        this.buckets = Array.from({ length: capacity }, () => new LinkedList());
+
+        // Reset size before reinsterting each key
+        this.size = 0;
+
+        for (const bucket of oldBuckets) {
+            for (const { key, value } of bucket) {
+                this.set(key, value);
+            }
+        }
     }
 }
